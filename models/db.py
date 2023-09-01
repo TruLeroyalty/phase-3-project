25f451bd7659824c08f4ee5befc52b8b7498922b
Leroy Scott Jr <leroysjr@yahoo.com>
2023-09-01 13:14:27 -0500

    Created columns

---
+++ models/db.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine,Column,Integer,String,ForeignKey
+from sqlalchemy.orm import declarative_base,sessionmaker
+Base=declarative_base()
+
+class User(Base):
+    __tablename__="users"
+
+    id = Column(Integer,primary_key=True)
+    name = Column(String, unique=True)
+    username = Column(String)
+    password = Column(String)
+    budget = Column(Integer)
