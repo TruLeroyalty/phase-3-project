1559106c4db9de422fe50d178155e52d7fe6e9c9
Leroy Scott Jr <leroysjr@yahoo.com>
2023-09-01 13:24:48 -0500

    Created the columns and also pre-connecting this table to the users table

--- models/models.py
+++ models/models.py
@@ -4,3 +4,10 @@ from db import Base
 
 class Expense(Base):
     __tablename__ = "expenses"
+
+    id = Column(Integer, primary_key=True)
+    description = Column(String)
+    amount = Column(Integer)
+    user_id = Column(Integer,ForeignKey("users.id"))
+
+    user = relationship("User", back_populates="expenses")
