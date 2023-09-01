ca10b1518c85b5572a63a2aad939210eeabb252a
Leroy Scott Jr <leroysjr@yahoo.com>
2023-09-01 13:34:18 -0500

    Starting registration process

--- models/cli.py
+++ models/cli.py
@@ -2,6 +2,13 @@ from db import Session,User,Base,Engine
 import click
 from prettycli import red,green,yellow
 
+def register_user(session):
+    click.echo("Register")
+
+
+
+
+
 def main():
     click.echo(yellow("Seems like you need help keeping track of your budgeting!"))
     session = Session()
