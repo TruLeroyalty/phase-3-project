b2330ebccc8ed4c10dd856569303985060cc90ad
Leroy Scott Jr <leroysjr@yahoo.com>
2023-09-01 13:30:16 -0500

    Giving them an option to login or register

--- models/cli.py
+++ models/cli.py
@@ -5,3 +5,6 @@ from prettycli import red,green,yellow
 def main():
     click.echo(yellow("Seems like you need help keeping track of your budgeting!"))
     session = Session()
+
+    choice = click.prompt(click.style("Do you want to (l)ogin or (r)egister?", fg="yellow"), type=click.Choice(["l", "r"]))
+        
